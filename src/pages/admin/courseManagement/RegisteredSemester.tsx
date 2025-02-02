14033850e3421783b6bb5d4a75bd148e5126f8d7
AlamRazek <ohidul933@gmail.com>
2025-02-02 21:28:14 +0000

    semester Registration added6

--- src/pages/admin/courseManagement/RegisteredSemester.tsx
+++ src/pages/admin/courseManagement/RegisteredSemester.tsx
@@ -1,8 +1,91 @@
+import { Button, Table, TableColumnsType, TableProps } from "antd";
+
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { useGetAllRegisteredSemestersQuery } from "../../../redux/features/admin/courseManagement";
+
+export type TTAbleData = Pick<
+  TAcademicSemester,
+  "name" | "year" | "startMonth" | "endMonth"
+>;
+
 const RegisteredSemester = () => {
+  // const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+
+  const {
+    data: semesterData,
+    isLoading,
+    isFetching,
+  } = useGetAllRegisteredSemestersQuery(undefined);
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, academicSemester, startDate, endDate, status }) => ({
+      key: _id,
+      name: academicSemester,
+      startDate,
+      endDate,
+      status,
+    })
+  );
+
+  const columns: TableColumnsType<TTAbleData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+    },
+
+    {
+      title: "Status",
+      key: "status",
+      dataIndex: "status",
+    },
+    {
+      title: "Start Date",
+      key: "startDate",
+      dataIndex: "startDate",
+    },
+    {
+      title: "End Date",
+      key: "endDate",
+      dataIndex: "endDate",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  // const onChange: TableProps<TTAbleData>["onChange"] = (
+  //   _pagination,
+  //   filters,
+  //   _sorter,
+  //   extra
+  // ) => {
+  //   if (extra.action === "filter") {
+  //     const queryParams: TQueryParam[] = [];
+  //     setParams(queryParams);
+  //   }
+  // };
+
+  // if (isLoading) {
+  //   return <p>Loading</p>;
+  // }
+
   return (
-    <div>
-      <h1>RegisteredSemester</h1>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      // onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
   );
 };
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./sidebar.types";
 export * from "./global";
 export * from "./userManagement.type";
+export * from "./courseManagement.types";
